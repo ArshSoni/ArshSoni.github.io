dd0b493a9592e22d02fb81289efa9ba631aed622
Arshdeep Soni <arshdsoni@gmail.com>
2016-11-20 11:52:58 +0000

    added new ms

---
+++ ms/scripts/main.js
@@ -0,0 +1,25 @@
+// if (document.readyState === 'complete') {
+//   var navbarSelector = '.js-navbar', 
+//     fixedClass = 'header-fixed',
+//     bodySelector = 'body', 
+
+//     navbar = document.querySelector(navbarSelector), 
+//     body = document.querySelector(bodySelector);
+
+//   body.on('click', function() {
+//     alert('helo');
+//   })
+
+
+// }
+
+// var navbarSelector = '.js-navbar', 
+//   fixedClass = 'header-fixed',
+//   bodySelector = 'body', 
+
+//   navbar = document.querySelector(navbarSelector), 
+//   body = document.querySelector(bodySelector);
+
+// body.addEventListener('scroll', function(e) {
+//   console.log('hello');
+// })
